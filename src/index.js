4a10f0f7e6a5c7b4650b57c3b5e0ca912490ba80
Toprak <toprak.bilici@gmail.com>
2024-06-16 22:30:42 +0300

    got rid of the error when trying to get a random movie out of a movie watchlist that consists only 1 page

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.get('/', (req,res) => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto(url, {waitUntil: 'domcontentloaded'});
-        await page.waitForSelector('#content', { timeout: 5_000 });
+        await page.waitForSelector('a', { timeout: 5_000 });
     
 
         links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), (e) => e.href));
@@ -40,29 +40,47 @@ app.get('/', (req,res) => {
         await runFirst();
         const pageLinks = links.filter((string) => string.includes('page'))
         const maxPageNumberString = await pageLinks[pageLinks.length - 1]
-        const maxPageNumber = await maxPageNumberString.match(/\d+/)[0];
-        let filmUrls = [];
-        let filmCount = 0;
-        for(let i = 1; i <= maxPageNumber; i++){
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            const watchlistPages = url + `page/${i}/`
-            await page.goto(watchlistPages, {waitUntil: 'domcontentloaded'});
-            await page.waitForSelector('#content', { timeout: 5_000 });
+        try {
+            let maxPageNumber = 0;
+            if (maxPageNumberString !== undefined) {
+                const matchResult = maxPageNumberString.match(/\d+/);
+                if (matchResult) {
+                    maxPageNumber = parseInt(matchResult[0], 10);
+                } else {
+                    maxPageNumber = 1; 
+                }
+            } else {
+                maxPageNumber = 1;
+            }
+            let filmUrls = [];
+            let filmCount = 0;
+            for(let i = 1; i <= maxPageNumber; i++){
+                const browser = await puppeteer.launch();
+                const page = await browser.newPage();
+                const watchlistPages = url + `page/${i}/`
+                await page.goto(watchlistPages, {waitUntil: 'networkidle0'});
+                await page.waitForSelector('.js-watchlist-main-content .poster-list .poster-container .poster div', { timeout: 10000 });
+        
+                const filmDatas = await page.evaluate(() => Array.from(document.querySelectorAll('.js-watchlist-main-content .poster-list .poster-container .poster div'), (e) => ({
+                    title: e.querySelector('a .frame-title').innerText,
+                    link: e.querySelector('a').href
+                })));
+                filmUrls = filmUrls.concat(filmDatas)
+                filmCount = filmUrls.length;
+                console.log(filmCount)
+                console.log(filmUrls)
+                await browser.close();
+        
+            }
     
-            const filmDatas = await page.evaluate(() => Array.from(document.querySelectorAll('.js-watchlist-main-content .poster-list .poster-container .poster div'), (e) => ({
-                title: e.querySelector('a .frame-title').innerText,
-                link: e.querySelector('a').href
-            })));
-            filmUrls = filmUrls.concat(filmDatas)
-            filmCount = filmUrls.length;
-            await browser.close();
-    
+        
+            let randomFilmIndex = Math.floor(Math.random() * (filmCount - 1) + 1);
+            return filmUrls[randomFilmIndex];
+            
+        } catch (error) {
+            console.log(error)
         }
 
-    
-        let randomFilmIndex = Math.floor(Math.random() * (filmCount - 1) + 1);
-        return filmUrls[randomFilmIndex];
     }
     (async () => {
         res.json(await randomFilmGetter());
